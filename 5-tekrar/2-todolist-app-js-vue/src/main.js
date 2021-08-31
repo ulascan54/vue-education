8b71430ca5594d8ee3f0d410f22856a7ae3ec776
Ulaş Can Demirbağ <ulascandemirbag@gmail.com>
2021-08-31 14:53:00 +0300

    proje is over ready for deploy

--- 5-tekrar/2-todolist-app-js-vue/src/main.js
+++ 5-tekrar/2-todolist-app-js-vue/src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import router from './router.js'
+import '@/assets/style.css'
+import { appAxios } from './utils/appAxios'
+const app = createApp(App)
+app.use(router)
+app.config.globalProperties.$appAxios=appAxios
+app.mount('#app')
