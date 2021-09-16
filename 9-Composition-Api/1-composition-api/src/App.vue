c29265908f5973fa8a7df497d13e21ee6b4ef35b
ulascan54 <ulascandemirbag@gmail.com>
2021-09-16 19:22:54 +0300

    composition api tekrar çalışması ref reactive effect

---
+++ 9-Composition-Api/1-composition-api/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<h3>{{title}}</h3>
+  <input type="text" v-model="title">    
+  <button @click="show=!show">TIk</button>
+  <p v-if="show">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quasi, veniam magnam velit dolorem nihil, quia ut delectus qui quisquam quas corporis nesciunt voluptatum numquam optio veritatis natus quis voluptatem.
+  </p>
+</template>
+<script>
+  import {ref} from 'vue'
+
+export default {
+  // data() {
+  //   return {
+        // title:"başlık"
+  //   }
+  // },
+  setup() {
+    const title=ref('Başlık');
+      const show=ref(true)
+    return{
+      title,
+      show,
+    }
+  }
+}
+</script>
